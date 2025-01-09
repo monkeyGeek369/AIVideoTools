e8f9807942ffd7036384024b0ec8818cb52c11c2
monkeygeek <monkeygeek@qq.com>
2025-01-10 00:15:02 +0800

    new file:   webui.py

---
+++ webui.py
@@ -0,0 +1,17 @@
+import streamlit as st
+import os,sys
+from uuid import uuid4
+
+def main():
+    st.header("AIVideoTools")
+    st.pills
+    
+
+
+
+
+if __name__ == "__main__":
+    main()
+
+
+
