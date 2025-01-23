c5329ada811eb24e82cc1d8cd15100bee67dd774
monkeygeek <monkeygeek@qq.com>
2025-01-23 22:41:48 +0800

    123456

--- webui/components/video_meta_data_settings.py
+++ webui/components/video_meta_data_settings.py
@@ -1,12 +1,20 @@
 from streamlit.delta_generator import DeltaGenerator
+import streamlit as st
+from app.utils import utils,file_utils
+import os
 
 def render_video_meta_data(tr,st_container:DeltaGenerator) -> dict[str,DeltaGenerator]:
     result = {}
+    task_path = st.session_state['task_path']
 
     # material video expander
     material_video_expander = st_container.expander(label=tr("material_video"),expanded=True)
     material_video_expander.write(tr("material_video_tips"))
     result['material_video_expander'] = material_video_expander
+    material_videos_path = os.path.join(task_path, "material_videos")
+    material_videos = file_utils.get_file_list(directory=material_videos_path)
+    for material_video in material_videos:
+        material_video_expander.video(material_video.path)
 
     # material bg music expander 
     #material_bg_music_expander = st_container.expander(label=tr("material_bg_music"),expanded=True)
@@ -17,11 +25,29 @@ def render_video_meta_data(tr,st_container:DeltaGenerator) -> dict[str,DeltaGene
     material_voice_expander = st_container.expander(label=tr("material_voice"),expanded=True)
     material_voice_expander.write(tr("material_voice_tips"))
     result['material_voice_expander'] = material_voice_expander
+    material_voices_path = os.path.join(task_path, "material_voices")
+    material_voices = file_utils.get_file_list(directory=material_voices_path)
+    for material_voice in material_voices:
+        material_voice_expander.audio(material_voice.path)
 
     # material subtitle expander
     material_subtitle_expander = st_container.expander(label=tr("material_subtitle"),expanded=True)
     material_subtitle_expander.write(tr("material_subtitle_tips"))
     result['material_subtitle_expander'] = material_subtitle_expander
+    material_subtitles_path = os.path.join(task_path, "material_subtitles")
+    material_subtitles = file_utils.get_file_list(directory=material_subtitles_path)
+    for material_subtitle in material_subtitles:
+        with open(material_subtitle.path, 'r', encoding='utf-8') as f:
+            subtitle_content = f.read()
+            material_subtitle_expander.text_area(
+                material_subtitle.name,
+                value=subtitle_content,
+                height=150,
+                label_visibility="collapsed",
+                key=material_subtitle.name
+            )
+
+
     return result
 
 
