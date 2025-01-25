0d4e0e8ab2a6f7ef8b8bde5aaaa384925c7f02f1
monkeygeek <monkeygeek@qq.com>
2025-01-25 11:22:16 +0800

    999

--- webui/components/video_handler_settings.py
+++ webui/components/video_handler_settings.py
@@ -3,28 +3,46 @@ import streamlit as st
 import os
 from moviepy.editor import VideoFileClip, concatenate_videoclips,vfx
 from app.utils import file_utils,utils
+from moviepy.video.fx.painting import painting
+import numpy as np
 
 
 def render_video_handler(tr,st_container:DeltaGenerator,container_dict:dict[str,DeltaGenerator]):
     # get cols
-    col1,col2 = st_container.columns(2)
+    col1,col2,col3 = st_container.columns(3)
+    
+    filter_options = [
+        (tr("none_filter"), ""),
+        (tr("warm_sun_filter"), "warm_sun"),
+        (tr("setting_sun_filter"), "setting_sun"),
+        (tr("sea_blue_filter"), "sea_blue"),
+    ]
 
     video_mirror = None
     video_stylize = None
+    video_filter = None
     with col1:
         video_mirror = col1.checkbox(label=tr("video_mirror"),key="video_mirror")
     with col2:
         video_stylize = col2.checkbox(label=tr("video_stylize"),key="video_stylize")
+    with col3:
+        video_filter_selected = col3.selectbox(
+            tr("video_filter"),
+            index=0,
+            options=range(len(filter_options)),
+            format_func=lambda x: filter_options[x][0],
+        )
+        video_filter = filter_options[video_filter_selected][1]
 
     # submit
     submit_button = st_container.button(tr("video_handler_submit"))
     if submit_button:
         with st_container:
             with st.spinner(tr("processing")):
-                render_video_edit(tr,st_container,container_dict,video_mirror,video_stylize)
+                render_video_edit(tr,st_container,container_dict,video_mirror,video_stylize,video_filter)
 
 
-def render_video_edit(tr,st_container:DeltaGenerator,container_dict:dict[str,DeltaGenerator],video_mirror:bool,video_stylize:bool):
+def render_video_edit(tr,st_container:DeltaGenerator,container_dict:dict[str,DeltaGenerator],video_mirror:bool,video_stylize:bool,video_filter:str):
     try:
         video_list = []
         task_path = st.session_state['task_path']
@@ -51,7 +69,11 @@ def render_video_edit(tr,st_container:DeltaGenerator,container_dict:dict[str,Del
 
         # stylize
         if video_stylize:
-            pass
+            final_clip = final_clip.fx(painting)
+
+        # filter
+        if video_filter and len(video_filter) > 0:
+            final_clip = video_filter_handler(final_clip,video_filter)
 
         # save
         edit_videos_path = os.path.join(task_path, "edit_videos")
@@ -65,6 +87,32 @@ def render_video_edit(tr,st_container:DeltaGenerator,container_dict:dict[str,Del
     except Exception as e:
         st_container.error(e)
 
+def video_filter_handler(clip:VideoFileClip,video_filter:str):
+    if video_filter == "warm_sun":
+        clip = clip.fl_image(warm_sun_filter)
+    if video_filter == "setting_sun":
+        clip = clip.fl_image(setting_sun_filter)
+    if video_filter == "sea_blue":
+        clip = clip.fl_image(sea_blue_filter)
+    return clip
 
+def warm_sun_filter(frame):
+    frame_copy = frame.copy()
+    frame_copy[:, :, 0] = np.clip(frame_copy[:, :, 0] * 1.15, 0, 255) # 红色
+    frame_copy[:, :, 1] = np.clip(frame_copy[:, :, 1] * 1.02, 0, 255) # 绿色
+    frame_copy[:, :, 2] = np.clip(frame_copy[:, :, 2] * 0.85, 0, 255) # 蓝色
+    return frame_copy
 
+def setting_sun_filter(frame):
+    frame_copy = frame.copy()
+    frame_copy[:, :, 0] = np.clip(frame_copy[:, :, 0] * 1.2, 0, 255) # 红色
+    frame_copy[:, :, 1] = np.clip(frame_copy[:, :, 1] * 1, 0, 255) # 绿色
+    frame_copy[:, :, 2] = np.clip(frame_copy[:, :, 2] * 0.9, 0, 255) # 蓝色
+    return frame_copy
 
+def sea_blue_filter(frame):
+    frame_copy = frame.copy()
+    frame_copy[:, :, 0] = np.clip(frame_copy[:, :, 0] * 0.8, 0, 255) # 红色
+    frame_copy[:, :, 1] = np.clip(frame_copy[:, :, 1] * 1.03, 0, 255) # 绿色
+    frame_copy[:, :, 2] = np.clip(frame_copy[:, :, 2] * 1.21, 0, 255) # 蓝色
+    return frame_copy
