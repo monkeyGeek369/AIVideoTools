a380b844f6dec4d7be18c9d787e67b19b1d03d96
马浩 <mahao@yunquna.com>
2025-01-26 12:01:40 +0800

    124354675

--- app/test/test_moviepy.py
+++ app/test/test_moviepy.py
@@ -2,11 +2,47 @@
 from moviepy.editor import VideoFileClip,AudioFileClip,CompositeAudioClip,TextClip,CompositeVideoClip
 
 
+def test_subtitle():
+   # load video
+   video_clip = VideoFileClip("/Users/monkeygeek/Downloads/test.mp4")
+
+   #colors = TextClip.list('color')
+   #print(colors)
+
+   #fonts = TextClip.list('font')
+   #print(fonts)
+
+   # subtitle test
+   txt_clip = TextClip(
+      txt="hello world , this is a subtitle",
+      size=(video_clip.size[0], None), # subtitle width and height(auto height)
+      fontsize=60,
+      bg_color="blue",
+      color="white",
+      #font="Arial", # font name from system and imageMagick
+      font="/Users/monkeygeek/Documents/softProject/AIVideoTools/resource/fonts/STHeitiMedium.ttc",
+      stroke_color="black",
+      stroke_width=1.5,
+      method="caption", # subtitle while drawn in a picture with fixed size
+      kerning=1, # letter spacing
+      align="center", # txt align 文本对齐方式
+      interline=None,# interline 设置行间距倍数
+      transparent=True,
+      remove_temp=True
+   )
+   #txt_clip = txt_clip.set_position('bottom').set_duration(video_clip.duration).set_start(0)
+   #txt_clip = txt_clip.set_position((45,150)).set_duration(video_clip.duration).set_start(0)
+   #txt_clip = txt_clip.set_position(("center","top")).set_duration(video_clip.duration).set_start(0)
+   #txt_clip = txt_clip.set_position((0.4,0.7), relative=True).set_duration(video_clip.duration).set_start(0)
+   txt_clip = txt_clip.set_position(lambda t: ('center', 50+t) ).set_duration(video_clip.duration).set_start(0) # x:center y:50px from top
+
+   # combine
+   result = CompositeVideoClip([video_clip, txt_clip])
+   result.write_videofile("/Users/monkeygeek/Downloads/output_video.mp4", fps=24)
 
 if __name__ == '__main__':
-   video_clip = VideoFileClip("F:\download\\test.mp4")
-   txt_clip = TextClip("Your Subtitle Text", fontsize=50, color='white')
-   txt_clip = txt_clip.set_position('bottom').set_duration(video_clip.duration).set_start(0)
-   result = CompositeVideoClip([video_clip, txt_clip])
-   result.write_videofile("F:\download\output_video.mp4", fps=24)
+
+   # test subtitle
+   test_subtitle()
+
 
