29214542d45c0ba2871dc24a0b8cddb9e178f832
马浩 <mahao@yunquna.com>
2025-04-15 21:47:12 +0800

    123

--- test_mosaic.py
+++ test_mosaic.py
@@ -4,11 +4,14 @@ import numpy as np
 np.set_printoptions(threshold=np.inf)  # 完全禁用大小限制
 from app.services import paddleocr
 from app.utils import file_utils
+import os
 
 
 # mac
-video0_path = "/Users/monkeygeek/Downloads/国外网友度假时偶遇只搁浅的小魔鬼鱼，好心收养后发现它可爱又会互动.webm"
+video0_path = "/Users/monkeygeek/Downloads/test.webm"
 image_frame_path = "/Users/monkeygeek/Downloads/tmp/frame"
+mosaic_image_path = "/Users/monkeygeek/Downloads/tmp/mosaic"
+tmp_path = "/Users/monkeygeek/Downloads/tmp"
 
 # windows
 # video0_path = "F:\download\\test.webm"
@@ -21,7 +24,7 @@ def test_video_frame():
 
 def test_mosaic_telea():
     # get all frames
-    files = file_utils.get_file_list(image_frame_path, ".png")
+    files = file_utils.get_file_list(mosaic_image_path, ".png")
     if not files:
         return
 
@@ -34,13 +37,18 @@ def test_mosaic_telea():
         mask = np.zeros(img.shape[:2], dtype=np.uint8)
         
         coordinates = paddleocr.get_frame_coordinates(file.path,False)
+        if not coordinates:
+            continue
         for bbox in coordinates:
             (x1, y1), (x2, y2) = bbox
             cv2.rectangle(mask, (x1, y1), (x2, y2), 255, -1)
         
         result = cv2.inpaint(img, mask, 3, cv2.INPAINT_TELEA)
+
+        save_path = os.path.join(tmp_path, file.name+"."+file.suffix)
         
-        cv2.imwrite(file.path, result)
+        cv2.imwrite(save_path, result)
 
 if __name__ == "__main__":
+    #test_video_frame()
     test_mosaic_telea()
