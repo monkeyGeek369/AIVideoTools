84fd122e47d9412f80909d0a03724c8ea7d6a881
马浩 <mahao@yunquna.com>
2025-04-15 21:59:04 +0800

    123

--- test_mosaic.py
+++ test_mosaic.py
@@ -49,6 +49,39 @@ def test_mosaic_telea():
         
         cv2.imwrite(save_path, result)
 
+def test_mosaic_navier():
+    files = file_utils.get_file_list(mosaic_image_path, ".png")
+    if not files:
+        return
+
+    for index,file in enumerate(files):
+        img = cv2.imread(file.path)
+
+        hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+        lower_red = np.array([0, 50, 50])
+        upper_red = np.array([10, 255, 255])
+        mask = cv2.inRange(hsv, lower_red, upper_red)
+        
+        # 3. 形态学处理优化掩码
+        kernel = np.ones((3,3), np.uint8)
+        mask = cv2.dilate(mask, kernel, iterations=1)
+        
+        if img is None or mask is None:
+            return
+        
+        coordinates = paddleocr.get_frame_coordinates(file.path,False)
+        if not coordinates:
+            continue
+        for bbox in coordinates:
+            (x1, y1), (x2, y2) = bbox
+            cv2.rectangle(mask, (x1, y1), (x2, y2), 255, -1)
+        
+        result = cv2.inpaint(img, mask, 3, cv2.INPAINT_NS)
+        
+        save_path = os.path.join(tmp_path, str(index)+"."+file.suffix)
+        cv2.imwrite(save_path, result)
+
 if __name__ == "__main__":
     #test_video_frame()
-    test_mosaic_telea()
+    #test_mosaic_telea()
+    test_mosaic_navier()
